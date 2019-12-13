cbf4e4f45feffa9f0ec2e0411aaa14406d4ec0a3
riley-imac <riley.tippetts@gmail.com>
2019-12-13 14:00:20 -0700

    Add color and spacing primatives

---
+++ src/components/primatives.js
@@ -0,0 +1,60 @@
+// Spacing
+export const spacing8 = "0.5rem"
+export const spacing12 = "0.75rem"
+export const spacing16 = "1rem"
+export const spacing24 = "1.5rem"
+export const spacing32 = "2rem"
+export const spacing48 = "3rem"
+export const spacing56 = "3.5rem"
+export const spacing64 = "4rem"
+export const spacing72 = "4.5rem"
+export const spacing80 = "5rem"
+export const spacing96 = "6rem"
+
+// Color
+export const colorNeutral100 = "#f7f9fA"
+export const colorNeutral200 = "#ebeef0"
+export const colorNeutral300 = "#d5dde0"
+export const colorNeutral400 = "#abbac2"
+export const colorNeutral500 = "#8398a3"
+export const colorNeutral600 = "#697f8a"
+export const colorNeutral700 = "#526b78" //Body
+export const colorNeutral800 = "#40545e"
+export const colorNeutral900 = "#34454d" //Heading
+
+export const colorPrimary0 = "#def4fc"
+export const colorPrimary100 = "#ace1f7"
+export const colorPrimary200 = "#55c3ec" //Default
+export const colorPrimary300 = "#3d98bf"
+export const colorPrimary400 = "#29698c"
+export const colorPrimary500 = "#1f4b6a"
+
+export const colorGreen0 = "#e2f5ea"
+export const colorGreen100 = "#bae7cc"
+export const colorGreen200 = "#00bc71" //Default
+export const colorGreen300 = "#00a362"
+export const colorGreen400 = "#007a49"
+export const colorGreen500 = "#00663d"
+
+export const colorViolet0 = "#f0e7f2"
+export const colorViolet100 = "#d1c4e3"
+export const colorViolet200 = "#9575bf"
+export const colorViolet300 = "#7f58b1" //Default
+export const colorViolet400 = "#673F9e"
+export const colorViolet500 = "#56388c"
+
+export const colorRed0 = "#ffeaef"
+export const colorRed100 = "#fecad3"
+export const colorRed200 = "#f04354" //Default
+export const colorRed300 = "#e71b3a"
+export const colorRed400 = "#c8002c"
+export const colorRed500 = "#b90020"
+
+export const colorYellow0 = "#fff8e1"
+export const colorYellow100 = "#ffecb3"
+export const colorYellow200 = "#ffd54f"
+export const colorYellow300 = "#ffc107" //Default
+export const colorYellow400 = "#ffa000"
+export const colorYellow500 = "#ff8100"
+
+// Font size
